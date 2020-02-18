34f0007ffa4838b2e4438ee84eaf2797caf752d0
AupaRick <60593664+AupaRick@users.noreply.github.com>
2020-02-18 13:05:14 +0800

    2020.02.18 - add sqlconnector

--- AupaWeb/Controllers/PostController.cs
+++ AupaWeb/Controllers/PostController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AupaWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,7 +10,16 @@ namespace AupaWeb.Controllers
 {
     public class PostController : Controller
     {
+
+        private PostDataObject postObject = new PostDataObject();
+        private readonly SQLServerConnector sqlServerConnector = new SQLServerConnector();
         public ActionResult AddNewPost()
+        {
+            ViewBag.Message = sqlServerConnector.GetHashCode();
+            return View();
+        }
+
+        public ActionResult Details(int? id)
         {
             return View();
         }
