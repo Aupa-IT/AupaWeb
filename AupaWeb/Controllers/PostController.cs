bb2a8b324554530d2949d72d0917f378880a1f2a
AupaRick <60593664+AupaRick@users.noreply.github.com>
2020-02-26 16:40:21 +0800

    add Insert, SELECT

--- AupaWeb/Controllers/PostController.cs
+++ AupaWeb/Controllers/PostController.cs
@@ -1,6 +1,7 @@
 ﻿using AupaWeb.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,8 +12,9 @@ namespace AupaWeb.Controllers
     {
         public ActionResult AddNewPost()
         {
+            SQLServerConnector sqlServerConnector = new SQLServerConnector();
             List<PostDataObject> listPosts = new List<PostDataObject>();
-            listPosts = getPosts();
+            listPosts = sqlServerConnector.getPostsList();
 
             ViewBag.ListOfPosts = listPosts;
             return View(listPosts);
@@ -25,18 +27,36 @@ namespace AupaWeb.Controllers
         // POST: Students/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "aaa01,aaa02,aaa03,aaa04")] PostDataObject postDataObject)
+        public ActionResult Create([Bind(Include = "aaa06,aaa07")] PostDataObject postDataObject)
         {
             SQLServerConnector sQLServer = new SQLServerConnector();
+            List<PostDataObject> listPosts = new List<PostDataObject>();
+            listPosts = getPosts();
             //if (ModelState.IsValid)
             //{
             //    return RedirectToAction("AddNewPost");
             //}
-            List<PostDataObject> listPosts = new List<PostDataObject>();
-            listPosts = getPosts();
-            listPosts.Add(postDataObject);
-            ViewBag.ListOfPosts = listPosts;
-            return View("AddNewPost", listPosts);
+            postDataObject.Aaa01 = DateTime.Now.ToString("yyyyMMddHHmmss");
+            postDataObject.Aaa02 = "";
+            postDataObject.Aaa03 = "TEST";
+            postDataObject.Aaa04 = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            postDataObject.Aaa05 = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            postDataObject.Aaa08 = "";
+
+            String result = sQLServer.InsertPostData(postDataObject);
+
+            if (result == "true"){
+                listPosts.Add(postDataObject);
+                ViewBag.ListOfPosts = listPosts;
+                return View("AddNewPost", listPosts);
+            }
+            else
+            {
+                ViewBag.ListOfPosts = listPosts;
+                return View("AddNewPost", listPosts);
+            }
+
+            
         }
 
 
@@ -49,21 +69,21 @@ namespace AupaWeb.Controllers
             fakePost.Aaa01 = "1234567";
             fakePost.Aaa02 = "First Fake Title";
             fakePost.Aaa03 = "First Fake Content";
-            fakePost.Aaa04 = DateTime.Now;
+            //fakePost.Aaa04 = DateTime.Now;
             fakePosts.Add(fakePost);
 
             fakePost = new PostDataObject();
             fakePost.Aaa01 = "7654321";
             fakePost.Aaa02 = "Second Fake Title";
             fakePost.Aaa03 = "Second Fake Content";
-            fakePost.Aaa04 = DateTime.Now;
+            //fakePost.Aaa04 = DateTime.Now;
             fakePosts.Add(fakePost);
 
             fakePost = new PostDataObject();
             fakePost.Aaa01 = "9876543";
             fakePost.Aaa02 = "Third Fake Title";
             fakePost.Aaa03 = "Third Fake Content";
-            fakePost.Aaa04 = DateTime.Now;
+            //fakePost.Aaa04 = DateTime.Now;
             fakePosts.Add(fakePost);
 
             return fakePosts;
