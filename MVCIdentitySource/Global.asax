b1659004120d89134e9012823a061f17c6640dd5
AupaRick <60593664+AupaRick@users.noreply.github.com>
2020-04-06 10:22:48 +0800

    Add Identity

---
+++ MVCIdentitySource/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCIdentitySource.MvcApplication" Language="C#" %>
---
+++ MVCIdentitySource/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCIdentitySource.Startup))]
+namespace MVCIdentitySource
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
